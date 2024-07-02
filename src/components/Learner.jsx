26c30265efb73c9173c03503bd1be4e82276e0f9
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-07-02 11:57:57 -0700

    Completed Assignment

---
+++ src/components/Learner.jsx
@@ -0,0 +1,10 @@
+function Learner({name, bio}) {
+  return (
+    <>
+      <h3>{name}</h3>
+      <p>{bio}</p>
+    </>
+  )
+}
+
+export default Learner
---
+++ src/components/Score.jsx
@@ -0,0 +1,7 @@
+function Score({ date, score }) {
+  return (
+      <pre>Date: {date}   Score: {score}</pre>
+  )
+}
+
+export default Score
